fc2b50833f4817f74516bd80dc939b6aad1842a2
Daniel Harte <norgeous@users.noreply.github.com>
2023-01-22 12:00:25 +0000

    map over priceData for each row in table

--- order/src/components/App.js
+++ order/src/components/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import priceData from '../price-data';
+
+const currencyFormat = new Intl.NumberFormat('en-GB', {
+  style: 'currency',
+  currency: 'GBP',
+});
 
 const App = () => {
-
   return (
-    <h1>App</h1>
+    <>
+      <h1>App</h1>
+
+      {/* shows the products to select from */}
+      <table>
+        {priceData.map(({ label, price }) => (
+          <tr>
+            <td>{label}</td>
+            <td>{currencyFormat.format(price)}</td>
+            <td>
+              <button
+                onClick={() => alert(`add to cart: ${label}`)}
+              >
+                Add to Cart
+              </button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </>
   );
 };
 
